fd8d7f84aeee3e484b4452ad8e53d0bff6ed0b74
mehdi_807 <mehdij807@gmail.com>
2024-08-28 13:25:57 +0330

    created a controller for product

---
+++ src/CRUD-Testing/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Testing.Controllers;
+[ApiController]
+[Route("api/[Controller]")]
+public class ProductController:ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService product)
+    {
+        _productService = product;
+    }
+    [HttpPost]
+    public IActionResult AddProduct(string name,double price,string description)
+    {
+        _productService.Add(name, price, description);
+        return NoContent();
+    }
+    [HttpPut]
+    public IActionResult UpdateProduct(int Id,string name,double price,string description)
+    {
+        _productService.Update(Id, name, price, description);
+        return NoContent();
+    }
+    [HttpDelete("{Id}")]
+    public IActionResult DeleteProduct(int Id)
+    {
+        _productService.Delete(Id);
+        return NoContent();
+    }
+}
