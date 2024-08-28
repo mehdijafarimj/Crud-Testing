6f4e9e264ddfa9c70fe0409a9c8c31d117d08176
mehdi_807 <mehdij807@gmail.com>
2024-08-28 14:32:32 +0330

    created DbContext class

---
+++ src/Infrastructure/CrudDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+public class CrudDbContext: DbContext
+{
+    public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
