0a7f77c5578cea016502fe6fbc8f8420cc601a18
mehdi_807 <mehdij807@gmail.com>
2024-08-28 13:26:50 +0330

    created a controller for user

---
+++ src/CRUD-Testing/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+
+namespace CRUD_Testing.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class UserController:ControllerBase
+{
+    private readonly IUserService _userService;
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    [HttpPost]
+    public IActionResult AddUser(string name, string lastName,int age,string address,string nationalCode)    
+    {
+        _userService.Add(name, lastName, age,address,nationalCode);
+        return NoContent(); 
+    }
+    
+    [HttpPut]
+    public IActionResult UpdateUser(int Id, string name, string lastName, int age, string address, string nationalCode)
+    {
+        _userService.Update(Id, name, lastName, age, address, nationalCode);
+        return NoContent();
+    }
+
+    [HttpDelete("{Id}")]
+    public IActionResult DeleteUser(int Id)
+    {
+        _userService.Delete(Id);
+        return NoContent();
+    }
+}
