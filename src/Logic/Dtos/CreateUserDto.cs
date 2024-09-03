5045100e7aac1c19c005a1a687efff3575b15fe4
mehdi_807 <mehdij807@gmail.com>
2024-09-03 12:32:34 +0330

    created dto folder for every entity

---
+++ src/Logic/Dtos/CreateUserDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Dtos;
+public class CreateUserDto
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+    public string NationalCode { get; set; }
+}
+public class UpdateUserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+    public string NationalCode { get; set; }
+}
