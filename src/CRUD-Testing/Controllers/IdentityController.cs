9b9990fb30446833e58ed544fff279f7743145e9
mehdi_807 <mehdij807@gmail.com>
2024-09-14 18:39:55 +0330

    added json web tokens to api

---
+++ src/CRUD-Testing/Controllers/IdentityController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Core;
+using Domain;
+using Logic;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace CRUD_Testing.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private IConfiguration _config;
+    public LoginController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpPost]
+    public IActionResult GenerateToken([FromBody] LoginRequest loginRequest)
+    {
+        //Logic for login process 
+        //If Login and password was correct then proceed to generate token.
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        
+        var secToken = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Issuer"],
+            null,
+            expires: DateTime.Now.AddMinutes(10),
+            signingCredentials: credentials);
+
+        var token = new JwtSecurityTokenHandler().WriteToken(secToken);
+
+        return Ok(token);
+    }
+}
